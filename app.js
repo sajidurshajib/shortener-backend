b9a8dc78caf088eadb2264d4e8a51398876dcb7a
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-04 10:46:08 +0600

    app.js setup

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello')
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+app.use(express.json())
+
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+module.exports = app
