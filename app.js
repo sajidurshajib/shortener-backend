38e3aa347ef4abf2e63e6fed90253b57589efea9
sajidurshajib <sajidur.inbox@gmail.com>
2021-10-04 23:29:17 +0600

    add comment

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const morgan = require('morgan')
 
+//express
 const app = express()
 app.use(express.json())
 
+//morgan
 console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+//route
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
+//export
 module.exports = app
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const dotenv = require('dotenv')
-
+//env import
 dotenv.config({ path: './config.env' })
 
+//app require
 const app = require('./app')
 
+//run server
 const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
