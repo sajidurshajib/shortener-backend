3781691fc7667ddbc3a8406bd01ea665438560f0
sajidurshajib <sajidur.inbox@gmail.com>
2021-10-16 23:50:29 +0600

    Joi install

--- src/routes/linkRoutes.js
+++ src/routes/linkRoutes.js
@@ -4,6 +4,12 @@ const router = express.Router()
 
 router
     .route('/')
-    .get((req, res) => res.json({ msg: 'Link route' }))
+    .get((req, res) => res.json({ msg: 'Link get route' }))
+    .post((req, res) => res.json({ msg: 'Link post route' }))
+router
+    .route('/:id')
+    .get((req, res) => res.json({ msg: 'Link id get route' }))
+    .patch((req, res) => res.json({ msg: 'Link id patch route' }))
+    .delete((req, res) => res.json({ msg: 'Link id delete route' }))
 
 module.exports = router
