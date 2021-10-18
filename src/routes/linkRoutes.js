1f350a640a610f49a007e65a4bd9ac93e1fb20c3
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-18 09:58:20 +0600

    param

--- src/routes/linkRoutes.js
+++ src/routes/linkRoutes.js
@@ -2,8 +2,18 @@ const express = require('express')
 
 const router = express.Router()
 
+router.param('id', (req, res, next, val) => {
+    console.log(val)
+    next()
+})
+
 router
     .route('/')
     .get((req, res) => res.json({ msg: 'Link route' }))
+
+router.route('/:id').get((req, res, next) => {
+    console.log(req.params.id)
+    res.end()
+})
 
 module.exports = router
